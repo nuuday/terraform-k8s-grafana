01bd71a85034a981a55d0cf8c2c5955c851cde87
Anders Christian Sørensen <asore@nuuday.dk>
2020-08-13 23:25:20 +0200

    fix: pin terraform-aws-* modules and support aws provider v3

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws        = ">= 3.0, < 4.0"
+    helm       = ">= 1.1"
+    kubernetes = ">= 1.11"
+  }
+}
+
 locals {
   chart_name    = "grafana"
   chart_version = var.chart_version
@@ -83,7 +91,7 @@ data aws_region "grafana" {}
 data aws_caller_identity "grafana" {}
 
 module "iam" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  source = "github.com/terraform-aws-modules/terraform-aws-iam//modules/iam-assumable-role-with-oidc?ref=v2.14.0"
 
   create_role                   = true
   role_name                     = "${local.release_name}-irsa-${random_id.grafana_rds.dec}"
@@ -112,7 +120,7 @@ resource "aws_iam_role_policy" "grafana" {
 }
 
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "github.com/terraform-aws-modules/terraform-aws-s3-bucket?ref=v1.10.0"
 
   bucket_prefix = local.bucket_prefix
   acl           = "private"
@@ -144,7 +152,7 @@ resource "random_password" "grafana_db_password" {
 }
 
 module "db" {
-  source = "terraform-aws-modules/rds/aws"
+  source = "github.com/terraform-aws-modules/terraform-aws-rds?ref=v2.18.0"
 
   identifier                      = "grafana${random_id.grafana_rds.dec}"
   engine                          = "postgres"
